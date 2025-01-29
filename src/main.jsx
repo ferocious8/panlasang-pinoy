aebafcda7e91017535358f84fe7a8d7387b81a4e
ferocious8 <gerenzmendoza08@gmail.com>
2025-01-30 00:40:12 +0800

    Fix GitHub Pages deployment configuration

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { CartProvider } from './context/CartContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </HashRouter>
   </React.StrictMode>,
 )
